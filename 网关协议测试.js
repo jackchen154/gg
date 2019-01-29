abe2176944dd869164e91f8acff55c6f7df9253b
jackchen154 <34428887+jackchen154@users.noreply.github.com>
2019-01-29 17:37:25 +0800

    update

--- 网关协议测试.js
+++ 网关协议测试.js
@@ -4,7 +4,7 @@ homebridge发送:
 device回复:
 {
 "cmd": "iam", 
-"ip": "192.168.12.41",
+"ip": "192.168.0.111",
 "port": "4322"
 }
 homebridge发送:
@@ -14,14 +14,15 @@ device回复:
     "cmd": "get_id_list_ack",
     "sid": "6409802da3ca",
     "token": "1234567812345678",
-    "data": "[11,12,15,16]"//传感器列表
+    "data": "[11,12,15]"
 }
+{"cmd": "get_id_list_ack", "sid": "6409802da3ca","token": "1234567812345678","data": "[11,12,15]"}
 
+//传感器列表
 homebridge发送:
 {"cmd":"read", "sid":"11"}//温湿度传感器
 {"cmd":"read", "sid":"12"}//插座
 {"cmd":"read", "sid":"15"}//开关
-{"cmd":"read", "sid":"16"}//
 //我要读取这些传感器的信息
 
 
@@ -35,6 +36,7 @@ homebridge发送:
 		  \"key\": \"bbf5624a816f7f2702512d6c1cd55e53\"
 		  }"
 }
+{"cmd":"write","model":"plug","sid":"12","data":"{\"status\":\"off\",\"key\": \"bbf5624a816f7f2702512d6c1cd55e53\"}"}
 //设备回复插座图标状态
 {
     "cmd": "return_read",
@@ -42,6 +44,8 @@ homebridge发送:
     "sid": "12",
     "data": "{\"status\": \"on\"}"
 }
+{"cmd": "return_read","model": "plug", "sid": "12","data": "{\"status\": \"on\"}"}
+
 //控制插座下发的数据
 {
     "cmd": "write",
@@ -69,6 +73,8 @@ homebridge发送:
     "sid": "15",
     "data": "{\"channel_0\": \"on\",\"channel_1\": \"off\"}"
 }
+{ "cmd": "return_read","model": "ctrl_neutral1", "sid": "15","data": "{\"channel_0\": \"on\",\"channel_1\": \"off\"}"}
+{"cmd":"write_ack","model":"ctrl_neutral2","sid":"158d0000123456","short_id":4343,"data":"{\"channel_0\":\"on\",\"channel_1\":\"off\"}"}
 //温湿度
 {
     "cmd": "return_read",
@@ -76,5 +82,5 @@ homebridge发送:
     "sid": "11",
     "data": "{\"temperature\": 2500,\"humidity\": 6501}"
 }
-
+{"cmd": "return_read", "model": "sensor_ht","sid": "11","data": "{\"temperature\": 2500,\"humidity\": 6501}"}
 
