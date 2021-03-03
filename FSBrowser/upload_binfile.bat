78719a038dc50487aa4887942e106388930e5513
jackchen154 <34428887+jackchen154@users.noreply.github.com>
2021-03-03 15:49:56 +0800

    genxin

--- FSBrowser/upload_binfile.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set bin_file=FSBrowser.ino.nodemcu.bin
-set upload_tool=C:\Users\Administrator\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\2.7.4\tools\upload.py
-
-python3 %upload_tool% --chip esp8266 --port COM27 --baud 921600 erase_flash --before default_reset --after hard_reset write_flash 0x0 %bin_file%
-pause
